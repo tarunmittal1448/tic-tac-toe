80d258ddde37b4522a4b1925cff1a900602bf16c
tarun-mittal-coder <tarun1448.be21@chitkara.edu.in>
2024-08-14 11:48:42 +0530

    final adding

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [mark, setMark] = useState([null,null,null,null,null,null,null,null,null]);
-  const [isNext, setIsNext]=useState(true);
-  const [winner, setWinner] = useState(null);
+  const [mark, setMark] = useState([null,null,null,null,null,null,null,null,null]); // saare null boxes h total 9
+  const [isNext, setIsNext]=useState(true); //next turn k liye use kr rhe h hum
+  const [winner, setWinner] = useState(null); // winner h ki nhi
 
-  const checkWinner = (marks) => {
+  const checkWinner = (marks) => { // marks added and combination checking
     const lines = [
       [0, 1, 2],
       [3, 4, 5],
@@ -15,15 +15,15 @@ function App() {
       [1, 4, 7],
       [2, 5, 8],
       [0, 4, 8],
-      [2, 4, 6],
+      [2, 4, 6],  // hmare saare possible combinations h jisse pass ho ske
     ];
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
-      if (marks[a] && marks[a] === marks[b] && marks[a] === marks[c]) {
+      if (marks[a] && marks[a] === marks[b] && marks[a] === marks[c]) {    // line checking whether the lines are on same or not
         return marks[a];
       }
     }
-    if (!marks.includes(null)) {
+    if (!marks.includes(null)) { //upper vale combinations k alava koi combination bnta ho toh draw krdega 
       return 'draw';
     }
     return null;
@@ -43,10 +43,10 @@ function App() {
 
   const renderButton = (index) => {
     return (
-      <button onClick={() => clickHandler(index)} style={{ width: "50px", height: "50px", border: "1px solid white", verticalAlign: "top", backgroundColor:"black", color:"white"}}>
+      <button onClick={() => clickHandler(index)} style={{width: "125px", height: "125px", border: "1px solid white", verticalAlign: "top", backgroundColor:"black", color:"white"}}>
         {mark[index]}
       </button>
-    );
+    ); 
   }
 
   const renderStatus = () => {
@@ -60,7 +60,8 @@ function App() {
   }
 
   return (
-    <>
+    <div style={{marginTop:"-300px", marginLeft:"240px"}}>
+      <div><b>{renderStatus()}</b></div>
       <div>
         {renderButton(0)}
         {renderButton(1)}
@@ -76,8 +77,7 @@ function App() {
         {renderButton(7)}
         {renderButton(8)}
       </div>
-      <div>{renderStatus()}</div>
-    </>
+    </div>
   );
 }
 
